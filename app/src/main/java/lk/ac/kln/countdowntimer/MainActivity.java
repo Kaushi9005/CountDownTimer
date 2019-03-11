ad7b1505ab13a158df9872de58e9def6d611e325
Kaushi9005 <kaushalyanadeeshani96@gmail.com>
2019-03-11 09:26:56 +0530

    Adjustment of rotation

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -7,19 +7,43 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int counter = 99;
+    private boolean running;
+    private boolean wasrunning;
+    private static final String CURRENT_COUNTER = "counter";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        countDown();
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(CURRENT_COUNTER);
+        }
+             countDown();
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle SavedInstanceState){
+        super.onSaveInstanceState(SavedInstanceState);
+        SavedInstanceState.putInt(CURRENT_COUNTER,counter);
+
+
+    }
+
+
     private void countDown(){
-        final TextView textview=findViewById(R.id.textView);
+
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
+                final TextView textview=findViewById(R.id.textView3);
                 textview.setText(Integer.toString(counter));
+                //Reset the counter
+                if (counter == 0) {
+                    counter = 99;
+                }
+
+
+
                 counter--;
                 handler.postDelayed(this,1000);
 
